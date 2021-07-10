09838f2cdfdf8ed8e276b715cbf86dc128c299fb
massytak <massi.messalti@gmail.com>
2021-07-10 10:28:46 +0200

    rest api initiation

--- rest/app.js
+++ rest/app.js
@@ -1,12 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const index = require("./routes/index");
+const session = require("express-session");
+const authRoutes = require("./routes/auth-routes");
 
 mongoose
   .connect('mongodb://localhost/rest', {useNewUrlParser: true,useUnifiedTopology: true})
@@ -42,12 +46,28 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+// ADD SESSION SETTINGS HERE:
+app.use(
+  session({
+    secret: "some secret goes here",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
 
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
+// ADD CORS SETTINGS HERE TO ALLOW CROSS-ORIGIN INTERACTION:
+const cors = require("cors");
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000"],
+  })
+);
 
 const index = require('./routes/index');
 app.use('/', index);
